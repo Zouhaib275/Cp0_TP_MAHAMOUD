d4d12b9211c052bd791cda6f6a4138f882094ba0
zouhaib mahamoud <zouhaib mahamoud@10.3.62.167>
2025-11-03 15:44:58 +0100

    Creation d'un nouveau projet Tp0_hello world  de Zouhaib

---
+++ TP0_HelloWorld/src/tp0_helloworld/TP0_HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+/**
+ *
+ * @author zouhaib mahamoud
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( " Bonjour " );
+        System.out.println( " Au revoir " );
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prenom ?");
+sc.nextLine(); 
+
+    }
+    
+}
